796cd8f5e4a36a22098ad394c5762af2feaa05bf
Dmitriy-Kulik <0663642996d@gmail.com>
2018-02-21 21:51:32 +0200

    переделал float и padding, добавил div btn_block в котором положил кнопки

---
+++ src/main.js
@@ -0,0 +1,52 @@
+var app = angular.module('app', []);
+
+app.controller('PageController', function($scope){
+    $scope.layout = 'grid';
+
+    $scope.itemClicked = function ($index) {
+        $scope.layout = $index;
+        return $scope.layout;
+    };
+
+    $scope.mysort = function ($index) {
+        $scope.sortmethod = $index;
+        return $scope.sortmethod;
+    }
+});
+
+app.controller('ProductsController', function(){
+    this.products = [
+        {
+            name: "TP-LINK TL-WR841N",
+            img: "img/tp_link_tl_wr841n_images_5837240.jpg",
+            price: "529",
+            presence: true
+        },
+        {
+            name: "Xiaomi WiFi MiRouter 3 White International Version",
+            img: "img/copy_xiaomi_wifi_mirouter_3_587e3a15bac14_images_1837337938.jpg",
+            price: "799",
+            presence: true
+        },
+        {
+            name: "TP-LINK TL-WR840N",
+            img: "img/tp-link_tl-wr840n_images_1551541342.jpg",
+            price: "463",
+            presence: false
+        },{
+            name: "Xiaomi Mini Wifi White",
+            img: "img/xiaomi_6954176868169_images_2313549793.jpg",
+            price: "749",
+            presence: true
+        },
+        {
+            name: "TP-LINK TL-WR940N",
+            img: "img/tp_link_tl_wr940n_images_1438460003.jpg",
+            price: "686",
+            presence: true
+        }
+    ]
+});
+
+
+
